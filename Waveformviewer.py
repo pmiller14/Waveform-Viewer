9c4d6755ebf5b3d8e3c0c3050512a1440d8145b9
pmiller14 <peter.k.miller12@gmail.com>
2022-02-09 09:15:54 -0600

    Update Waveformviewer.py
    This updated script for viewing time series data includes functionality to pick first arrivals using a thresholding method. The first arrival is picked when the waveform exceeds 10 times the standard deviation of the pre-signal noise level.

--- Waveformviewer.py
+++ Waveformviewer.py
@@ -2,29 +2,56 @@
 #import select libraries
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 from scipy import signal
+
+#computes the threshold value for the noise prior to P-arrival and after the signal
+#impulse response
+def get_threshold(detrended_signal,min_value,max_value,Post_impulse):
+    threshold = 10*np.std(detrended_signal[min_value:max_value])
+    Index = np.min(np.where(np.abs(detrended_signal[Post_impulse:])>threshold)[0])+Post_impulse
+    return threshold,Index
+
+#Converts an index to time (in microseconds)
+def rec2time(index,fs):
+    #returns time in seconds
+    time = ((index)/(fs))*1e6
+    return time
+
+fs = 50000000.                   #Sampling frequency 50 MHz
+min_value = 200
+max_value = 300
+Post_impulse = 300
+cutoff_index = 6000
 
 #Input stage number
 stage  = int(input('Stage:'))
-file = 'stage%drec5.txt' %stage
+file = 'stage%drec4.txt' %stage
 
 #load waveform
 wf = np.loadtxt(file,skiprows = 0)
-#wf1 = np.loadtxt('stage29rec5.txt',skiprows = 0)
 
 #Detrend waveform
-wf = signal.detrend(wf)
-#wf1 = signal.detrend(wf1)
+wf = signal.detrend(wf[0:cutoff_index])
+
+#Compute threshold and arrival index
+threshold, arrival = get_threshold(wf, min_value,max_value,Post_impulse)
+P_arrival_time = rec2time(arrival,fs)
+print("P arrival index: %0.1f" %arrival)
+print("P arrival time: %0.2f microseconds" %P_arrival_time)
 
 #Set up x axis and time if necessary
 x = np.linspace(0,wf.size-1,num=wf.size)
-time = (x[:] - 513)*0.02
+time = (x[:]/fs)*1e6
 
 fig = plt.figure(figsize=(15,8))
 ax = fig.add_subplot(1,1,1)
 ax.plot(x[:],wf[:],color = 'b', linewidth = 2)
-#ax.plot(x[:],wf1[:],color = 'r', linewidth = 2)
+ax.axhline(y=threshold,xmin=0,xmax=500,c='k',linewidth=2,label = '+threshold')
+ax.axhline(y=-threshold,xmin=0,xmax=500,c='k',linewidth=2,label = '-threshold')
+ax.set_xlim(0,2000)
+
 plt.grid()
 plt.show()
+
 #Editing test for github. 
+#copyright Peter Miller
